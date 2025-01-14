94586513f6b0161dbf979fa7737395b28345dc98
weak_ptr <weak_ptr@outlook.com>
2025-01-14 18:31:03 +0800

    chore: destroy after click

--- src/my-window.c
+++ src/my-window.c
@@ -8,8 +8,9 @@ struct _MyWindow {
 
 G_DEFINE_FINAL_TYPE(MyWindow, my_window, GTK_TYPE_APPLICATION_WINDOW)
 
-static void on_button_click(GtkEntry* entry, MyWindow* self) {
+static void on_button_click(GtkEntry *entry, MyWindow *self) {
   g_print("clicked\n");
+  gtk_window_destroy(GTK_WINDOW(self));
 }
 
 static void my_window_class_init(MyWindowClass *klass) {
