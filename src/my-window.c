a69a8dce10e5946afd5e77d8054b23f228b2477c
weak_ptr <weak_ptr@outlook.com>
2025-01-15 17:41:53 +0800

    chore: learning blueprint-compiler

--- src/my-window.c
+++ src/my-window.c
@@ -9,7 +9,7 @@ struct _MyWindow {
 G_DEFINE_FINAL_TYPE(MyWindow, my_window, ADW_TYPE_APPLICATION_WINDOW)
 
 static void my_window_class_init(MyWindowClass *klass) {
-  gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(klass), "/site/weakptr/fucksgiven/my-window.ui");
+  gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(klass), "/site/weakptr/fucksgiven/src/my-window.ui");
 }
 
 static void my_window_init(MyWindow *self) {
